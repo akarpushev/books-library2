c4fa3bb77b6bddac9d319e49d015738976ea0b93
Алексей <akarpushev@yandex.ru>
2024-03-25 02:11:44 +0300

    25.03.24

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from books import models
+
+
+@admin.register(models.Book)
+class BookAdmin(admin.ModelAdmin):
+    ...
+
--- books/models.py
+++ books/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    class TypeChoices(models.TextChoices):
+        HARDCOVER = "HB", "Твердый переплет"
+        PAPERCOVER = "PB", "Мягкий переплет"
+        EBOOK = "EB", "Электронная книга"
+    title = models.CharField(max_length=100)
+    year = models.PositiveSmallIntegerField()
+    description = models.TextField(default="")
+    book_type = models.CharField(max_length=2, choices=TypeChoices.choices, default=TypeChoices.PAPERCOVER)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=100, unique=True,default="")
+    #datetime.datetime(year=1970, month=1, day=1)
+
+    def __str__(self):
+        return self.title
+
+
